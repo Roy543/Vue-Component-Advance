6174ebf64930b65f026f4fd7e1b9f57df8130509
Roy543 <sahilsas88@gmail.com>
2023-04-23 12:58:04 +0200

    Vue Component advanced first commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>E-Mail:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["name", "phoneNumber", "emailAddress"],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "sahil",
+        name: "Sahil Saspara",
+        phone: "1234567890",
+        email: "sahil@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
